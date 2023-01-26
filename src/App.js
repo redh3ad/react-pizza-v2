b1c62d391df6418bf9c702f2b24e39effa75cca8
Егор Матяс <play.filin@gmail.com>
2023-01-26 23:15:22 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
@@ -7,9 +7,11 @@ import { Routes, Route } from 'react-router-dom';
 import './scss/app.scss';
 
 function App() {
+  const [searchValue, setSearchValue] = useState('');
+
   return (
     <div className='wrapper'>
-      <Header />
+      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
       <div className='content'>
         <Routes>
           <Route path='/' element={<Home />} />
--- src/components/Search/Index.jsx
+++ src/components/Search/Index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Search.module.scss';
 
-export default function Search() {
+export default function Search({ searchValue, setSearchValue }) {
   return (
     <div className={styles.root}>
       <svg
@@ -16,7 +16,44 @@ export default function Search() {
           id='XMLID_223_'
         />
       </svg>
-      <input className={styles.input} placeholder='Поиск пиццы...' />
+      <input
+        value={searchValue}
+        className={styles.input}
+        placeholder='Поиск пиццы...'
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      {searchValue && (
+        <svg
+          onClick={() => setSearchValue('')}
+          className={styles.clearIcon}
+          height='14px'
+          version='1.1'
+          viewBox='0 0 14 14'
+          width='14px'
+          xmlns='http://www.w3.org/2000/svg'>
+          <title />
+          <desc />
+          <defs />
+          <g
+            fill='none'
+            fill-rule='evenodd'
+            id='Page-1'
+            stroke='none'
+            stroke-width='1'>
+            <g
+              fill='#000000'
+              id='Core'
+              transform='translate(-341.000000, -89.000000)'>
+              <g id='close' transform='translate(341.000000, 89.000000)'>
+                <path
+                  d='M14,1.4 L12.6,0 L7,5.6 L1.4,0 L0,1.4 L5.6,7 L0,12.6 L1.4,14 L7,8.4 L12.6,14 L14,12.6 L8.4,7 L14,1.4 Z'
+                  id='Shape'
+                />
+              </g>
+            </g>
+          </g>
+        </svg>
+      )}
     </div>
   );
 }
