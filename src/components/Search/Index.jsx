f067689ab3070e1cc1c6d6c5cc81a9ca58373fde
Егор Матяс <play.filin@gmail.com>
2023-01-26 23:21:07 +0300

    feat: change svg prop

--- src/components/Search/Index.jsx
+++ src/components/Search/Index.jsx
@@ -6,7 +6,7 @@ export default function Search({ searchValue, setSearchValue }) {
     <div className={styles.root}>
       <svg
         className={styles.icon}
-        enable-background='new 0 0 32 32'
+        enableBackground='new 0 0 32 32'
         id='Glyph'
         version='1.1'
         viewBox='0 0 32 32'
