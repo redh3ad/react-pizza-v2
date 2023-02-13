f80479dd2da2cc74c32ca5d3167da500b916d77a
Егор Матяс <play.filin@gmail.com>
2023-02-13 13:23:52 +0300

    feat:use clsx for stylied button minus and add disabled when pizza count  equals one

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import clsx from 'clsx';
 import {
   addItem,
   removeItem,
@@ -58,9 +59,13 @@ const CartItem: React.FC<CartItemProps> = ({
         </p>
       </div>
       <div className='cart__item-count'>
-        <div
+        <button
+          disabled={count === 1}
           onClick={onClickMinus}
-          className='button button--outline button--circle cart__item-count-minus'>
+          className={clsx(
+            'button button--outline button--circle cart__item-count-minus',
+            { 'cart__item-count-minus--disabled': count === 1 },
+          )}>
           <svg
             width='10'
             height='10'
@@ -76,9 +81,9 @@ const CartItem: React.FC<CartItemProps> = ({
               fill='#EB5A1E'
             />
           </svg>
-        </div>
+        </button>
         <b>{count}</b>
-        <div
+        <button
           onClick={onClickPlus}
           className='button button--outline button--circle cart__item-count-plus'>
           <svg
@@ -96,7 +101,7 @@ const CartItem: React.FC<CartItemProps> = ({
               fill='#EB5A1E'
             />
           </svg>
-        </div>
+        </button>
       </div>
       <div className='cart__item-price'>
         <b>{price * count} ₽</b>
