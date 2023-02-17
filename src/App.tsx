f7416a88094c7092442119d4f9dde229c5db985a
Егор Матяс <play.filin@gmail.com>
2023-02-17 16:02:23 +0300

    feat: added lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import Home from './pages/Home';
-import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
 import FullPizza from './pages/FullPizza';
 import MainLayout from './layouts/MainLayout';
 import { Routes, Route } from 'react-router-dom';
 import './scss/app.scss';
 
+const Cart = lazy(() => import(/*webpackChunkName: "Cart"*/ './pages/Cart'));
+
 function App() {
   return (
     <Routes>
       <Route path='/' element={<MainLayout />}>
         <Route path='/' element={<Home />} />
-        <Route path='/cart' element={<Cart />} />
+        <Route
+          path='/cart'
+          element={
+            <Suspense fallback={<div>Загрузка корзины...</div>}>
+              <Cart />
+            </Suspense>
+          }
+        />
         <Route path='/pizza/:id' element={<FullPizza />} />
         <Route path='*' element={<NotFound />} />
       </Route>
