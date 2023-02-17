7411ab8f1f45000cde05d326ba66d8e4bcd81213
Егор Матяс <play.filin@gmail.com>
2023-02-17 21:57:58 +0300

    feat: reorganized imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,14 @@ import qs from 'qs';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-import NotFound from './NotFound';
-
-import Categories from '../components/Categories';
-import Sort from '../components/Sort';
-import PizzaBlock from '../components/PizzaBlock';
-import Skeleton from '../components/PizzaBlock/Skeleton';
-import Pagination from '../components/Pagination';
+import {
+  Categories,
+  Sort,
+  PizzaBlock,
+  Skeleton,
+  Pagination,
+  NotFound,
+} from '../components';
 
 import { useRef } from 'react';
 import { useAppDispatch } from '../redux/store';
