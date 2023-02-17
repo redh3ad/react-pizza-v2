61a961e9e66f684127df7d64563785aac4647669
Егор Матяс <play.filin@gmail.com>
2023-02-18 01:57:24 +0300

    test

--- README.md
+++
@@ -1,3 +0,0 @@
-# Link on deploy
-
-https://redh3ad.github.io/react-pizza-v2/
--- src/utils/calcTotalPrice.ts
+++
@@ -1,5 +0,0 @@
-import { TCartItem } from '../redux/slices/cart/types';
-
-export const calcTotalPrice = (items: TCartItem[]) => {
-  return items.reduce((sum, obj) => obj.price * obj.count + sum, 0);
-};
