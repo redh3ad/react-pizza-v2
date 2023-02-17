8e0f6052101a020f50fe17e7c9f1b7709590bb64
Егор Матяс <play.filin@gmail.com>
2023-02-17 16:21:11 +0300

    feat: changed lazy loading on react-loadable library

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { lazy, Suspense } from 'react';
+import Loadable from 'react-loadable';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import FullPizza from './pages/FullPizza';
@@ -6,21 +7,19 @@ import MainLayout from './layouts/MainLayout';
 import { Routes, Route } from 'react-router-dom';
 import './scss/app.scss';
 
-const Cart = lazy(() => import(/*webpackChunkName: "Cart"*/ './pages/Cart'));
+// const Cart = lazy(() => import(/*webpackChunkName: "Cart"*/ './pages/Cart'));
+
+const Cart = Loadable({
+  loader: () => import('./pages/Cart'),
+  loading: () => <div>Загрузка корзины...</div>,
+});
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<MainLayout />}>
         <Route path='/' element={<Home />} />
-        <Route
-          path='/cart'
-          element={
-            <Suspense fallback={<div>Загрузка корзины...</div>}>
-              <Cart />
-            </Suspense>
-          }
-        />
+        <Route path='/cart' element={<Cart />} />
         <Route path='/pizza/:id' element={<FullPizza />} />
         <Route path='*' element={<NotFound />} />
       </Route>
