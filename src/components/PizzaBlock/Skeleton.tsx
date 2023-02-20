a8b281efbd9179a0ed550a2f54ed698ce0b00df1
Егор Матяс <play.filin@gmail.com>
2023-02-20 11:59:26 +0300

    fix: fixed pizza-block-skeleton

--- src/components/PizzaBlock/Skeleton.tsx
+++ src/components/PizzaBlock/Skeleton.tsx
@@ -3,14 +3,14 @@ import ContentLoader from 'react-content-loader';
 
 const Skeleton: React.FC = () => (
   <ContentLoader
-    className='pizza-block'
+    className='pizza-block pizza-block-skeleton'
     speed={2}
     width={280}
     height={500}
     viewBox='0 0 280 500'
     backgroundColor='#f3f3f3'
     foregroundColor='#ecebeb'>
-    <circle cx='134' cy='136' r='125' />
+    <circle cx='134' cy='130' r='125' />
     <rect x='0' y='279' rx='10' ry='10' width='280' height='23' />
     <rect x='0' y='326' rx='10' ry='10' width='280' height='88' />
     <rect x='0' y='436' rx='10' ry='10' width='95' height='30' />
